7a6cd238c6a301f67941ce0dd39d9d1d9af9928a
oscar <oscarterraria.almando@gmail.com>
2024-09-11 13:17:55 +0700

    update_path

--- submission/dashboard/dashboard.py
+++ submission/dashboard/dashboard.py
@@ -6,6 +6,7 @@ import geopandas as gpd
 from shapely.geometry import Point
 from geopandas import GeoDataFrame
 import streamlit as st
+from pathlib import Path
 
 def show_top_category(df):
     colors = ['grey' if value != max(df['product_id'].head(10)) else 'blue' for value in df['product_id'].head(10)]
@@ -58,7 +59,7 @@ def show_marker_map(df):
 
     gdf.set_crs(epsg=4326, inplace=True)
 
-    world = gpd.read_file('main_data\country_map\\ne_10m_admin_0_countries.shp')
+    world = gpd.read_file(parent_dir/'main_data\country_map\\ne_10m_admin_0_countries.shp')
 
     brazil = world[world.SOVEREIGNT == 'Brazil']
 
@@ -113,10 +114,12 @@ def show_top_category_in_state(df, state_name):
     
     st.pyplot(plt)
 
-items_orders_products_payments_df = pd.read_csv('main_data\\items_orders_products_data.csv')
-state_mark_customer_count_df = pd.read_csv('main_data\\state_mark_customer_count_data.csv')
-order_city_count_df = pd.read_csv('main_data\\order_city_count_data.csv')
-product_in_state = pd.read_csv('main_data\\product_in_state_data.csv').sort_values(by='product_id', ascending=False)
+parent_dir = Path(__file__).parents[0]
+
+items_orders_products_payments_df = pd.read_csv(parent_dir/'main_data\items_orders_products_data.csv')
+state_mark_customer_count_df = pd.read_csv(parent_dir/'main_data\state_mark_customer_count_data.csv')
+order_city_count_df = pd.read_csv(parent_dir/'main_data\order_city_count_data.csv')
+product_in_state = pd.read_csv(parent_dir/'main_data\product_in_state_data.csv').sort_values(by='product_id', ascending=False)
 
 products_count_df = items_orders_products_payments_df.groupby(by='product_category_name_english').product_id.nunique().sort_values(ascending=False).reset_index()
 payments_type_count = items_orders_products_payments_df.groupby(by='payment_type').order_id.nunique().sort_values(ascending=False).reset_index()
